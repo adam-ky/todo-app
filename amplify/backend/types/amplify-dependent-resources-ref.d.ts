37ddde602e04eecb3941885f9adf5ccccd1ed03a
adam-ky <69505439+adam-ky@users.noreply.github.com>
2022-09-17 23:49:42 +0800

    add backend files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "taskTodoLF": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "TaskTodo": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
