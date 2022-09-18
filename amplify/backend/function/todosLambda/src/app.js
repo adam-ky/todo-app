db24d01a529725097aa573748311b846375de054
adam-ky <69505439+adam-ky@users.noreply.github.com>
2022-09-18 12:47:52 +0800

    integrate dynamodb

---
+++ amplify/backend/function/todosLambda/src/app.js
@@ -0,0 +1,151 @@
+const AWS = require("aws-sdk");
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+const bodyParser = require("body-parser");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+console.log(process.env.TABLE_REGION);
+AWS.config.update({ region: "ap-southeast-1" });
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+let tableName = "todosTable";
+if (process.env.ENV && process.env.ENV !== "NONE") {
+  tableName = tableName + "-" + process.env.ENV;
+}
+
+// declare a new express app
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get("/todos", function (req, res) {
+  let params = {
+    TableName: tableName,
+    limit: 100,
+  };
+  dynamodb.scan(params, (err, result) => {
+    if (err) {
+      res.json({ statusCode: 500, error: err.message });
+    } else {
+      res.json({
+        statusCode: 200,
+        url: req.url,
+        body: JSON.stringify(result.Items),
+      });
+    }
+  });
+});
+
+app.get("/todos/:id", function (req, res) {
+  let params = {
+    TableName: tableName,
+    Key: {
+      id: req.params.id,
+    },
+  };
+  dynamodb.get(params, (err, result) => {
+    if (err) {
+      res.json({ statusCode: 500, error: err.message });
+    } else {
+      res.json({
+        statusCode: 200,
+        url: req.url,
+        body: JSON.stringify(result.Item),
+      });
+    }
+  });
+});
+
+app.post("/todos", function (req, res) {
+  let params = {
+    TableName: tableName,
+    Item: {
+      id: uuidv4(),
+      ...req.body,
+      isComplete: false,
+    },
+  };
+
+  console.log(params);
+  dynamodb.put(params, err => {
+    if (err) {
+      res.json({ statusCode: 500, error: err.message, url: req.url });
+    } else {
+      res.json({
+        statusCode: 200,
+        url: req.url,
+        body: JSON.stringify(params.Item),
+      });
+    }
+  });
+});
+
+app.put("/todos", function (req, res) {
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: req.body.id,
+    },
+    ExpressionAttributeValues: {},
+    ReturnValues: "UPDATED_NEW",
+  };
+  params.UpdateExpression = "SET ";
+  if (req.body.description) {
+    params.ExpressionAttributeValues[":description"] = req.body.description;
+    params.UpdateExpression += "description = :description ";
+  }
+  if (req.body.isComplete) {
+    params.ExpressionAttributeValues[":isComplete"] = req.body.isComplete;
+    params.UpdateExpression += "isComplete = :isComplete ";
+  }
+  console.log(params.UpdateExpression);
+  dynamodb.update(params, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.json({ statusCode: 500, error: err.message, url: req.url });
+    } else {
+      res.json({
+        statusCode: 200,
+        url: req.url,
+        body: JSON.stringify(result.Attributes),
+      });
+    }
+  });
+});
+
+app.delete("/todos/:id", function (req, res) {
+  let params = {
+    TableName: tableName,
+    Key: {
+      id: req.params.id,
+    },
+  };
+  dynamodb.delete(params, (err, result) => {
+    if (err) {
+      res.json({ statusCode: 500, error: err.message, url: req.url });
+    } else {
+      res.json({
+        statusCode: 200,
+        url: req.url,
+        body: JSON.stringify(result),
+      });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("App started");
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Amplify, API } from "aws-amplify";
 import awsconfig from "./aws-exports";
@@ -7,42 +6,30 @@ import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
 
 function App() {
-  const [myMessage, setMyMessage] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-  // useEffect(() => {
-  //   exampleFectch();
-  // }, []);
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-  // async function exampleFectch() {
-  //   API.get("TaskTodo", "/tasks", {})
-  //     .then(res => {
-  //       setMyMessage(res.success);
-  //       console.log(`Response: ${res}`);
-  //     })
-  //     .catch(err => {
-  //       console.log(err.response);
-  //     });
-  // }
+  async function fetchTodos() {
+    API.get("todosApi", "/todos", {})
+      .then(res => {
+        console.log(res.body);
+        setTodoList(JSON.parse(res.body));
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>{myMessage}</p>
-    </div>
-  );
+  const test = () => {
+    console.log(todoList);
+    console.log("todo list has type of:", typeof todoList);
+    return todoList.map(todo => <p>{todo.description}</p>);
+  };
+
+  return <div>{test()}</div>;
 }
 
 export default App;
